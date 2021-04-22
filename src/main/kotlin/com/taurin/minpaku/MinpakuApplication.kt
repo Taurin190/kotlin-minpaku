8031199193de9cf3e88c6056092d30f7111ae105
Koichi Taura <taura.koichi@gmail.com>
2021-04-23 00:27:15 +0900

    add initial files from initializr

---
+++ src/main/kotlin/com/taurin/minpaku/MinpakuApplication.kt
@@ -0,0 +1,11 @@
+package com.taurin.minpaku
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MinpakuApplication
+
+fun main(args: Array<String>) {
+	runApplication<MinpakuApplication>(*args)
+}
---
+++ src/test/kotlin/com/taurin/minpaku/MinpakuApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.taurin.minpaku
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MinpakuApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
