b838fb40419da2c92740a1c5f27ab5e67769fc6e
Koichi Taura <taura.koichi@gmail.com>
2021-04-23 23:50:30 +0900

    add first controller connected with view

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,9 +19,12 @@ repositories {
 
 dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	implementation("org.springframework.boot:spring-boot-starter-thymeleaf")
+	implementation("org.springframework.boot:spring-boot-devtools")
 }
 
 tasks.withType<KotlinCompile> {
