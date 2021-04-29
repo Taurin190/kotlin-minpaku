3b5ae72dedfb66875863e7828e9fac3a60cb5ef1
Koichi Taura <taura.koichi@gmail.com>
2021-04-29 10:01:42 +0900

    add user service and repository

---
+++ src/main/kotlin/com/taurin/minpaku/Repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.taurin.minpaku.Repository
+
+import com.taurin.minpaku.Entity.User
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface UserRepository : JpaRepository<User, Long> {
+}
---
+++ src/main/kotlin/com/taurin/minpaku/Service/AuthService.kt
@@ -0,0 +1,12 @@
+package com.taurin.minpaku.Service
+
+import com.taurin.minpaku.Repository.UserRepository
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class AuthService(@Autowired userRepository: UserRepository) {
+    fun login(username: String, password: String) {
+
+    }
+}
