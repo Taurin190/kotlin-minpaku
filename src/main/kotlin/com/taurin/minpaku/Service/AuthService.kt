15699d5bfb93950a72fa844a8e39fd0eb12e1bda
Koichi Taura <taura.koichi@gmail.com>
2021-04-30 00:02:30 +0900

    add loadUserByUserName on AuthService

--- src/main/kotlin/com/taurin/minpaku/Service/AuthService.kt
+++ src/main/kotlin/com/taurin/minpaku/Service/AuthService.kt
@@ -1,5 +1,6 @@
 package com.taurin.minpaku.Service
 
+import com.taurin.minpaku.Dto.UserDto
 import com.taurin.minpaku.Repository.UserRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
@@ -8,5 +9,9 @@ import org.springframework.stereotype.Service
 class AuthService(@Autowired userRepository: UserRepository) {
     fun login(username: String, password: String) {
 
+    }
+
+    fun loadUserByUserName(userName: String): UserDto {
+        return UserDto()
     }
 }
--- src/test/kotlin/com/taurin/minpaku/Unit/AuthServiceTest.kt
+++ src/test/kotlin/com/taurin/minpaku/Unit/AuthServiceTest.kt
@@ -1,10 +1,34 @@
 package com.taurin.minpaku.Unit
 
+import com.ninjasquad.springmockk.MockkBean
+import com.taurin.minpaku.Entity.User
+import com.taurin.minpaku.Repository.UserRepository
+import com.taurin.minpaku.Service.AuthService
+import io.mockk.MockKAnnotations
+import io.mockk.every
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
+@ExtendWith(SpringExtension::class)
+@SpringBootTest
 class AuthServiceTest {
-    @Test
-    fun testLogin() {
+    @MockkBean
+    private lateinit var userRepository: UserRepository
 
+    @BeforeEach
+    fun setUp() = MockKAnnotations.init(this)
+
+    @Test
+    fun testLoadUserByUserName() {
+        val testUser = User(1, "testuser", "password", 1)
+        every { userRepository.findByUserName() } returns testUser
+        val authService = AuthService(userRepository)
+        val actual = authService.loadUserByUserName("testuser")
+        assertTrue(actual is UserDetails)
     }
 }
