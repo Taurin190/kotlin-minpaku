f1752d56817308ab391651db3f611ac1e6ac5a97
Koichi Taura <taura.koichi@gmail.com>
2021-05-09 16:58:47 +0900

    add smoke test on auth controller

--- src/test/kotlin/com/taurin/minpaku/Integration/AuthControllerTest.kt
+++ src/test/kotlin/com/taurin/minpaku/Integration/AuthControllerTest.kt
@@ -1,5 +1,7 @@
 package com.taurin.minpaku.Integration
 
+import com.taurin.minpaku.Form.LoginForm
+import com.taurin.minpaku.Form.RegisterForm
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,7 +12,7 @@ import org.springframework.http.HttpStatus
 
 @SpringBootTest(
     webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-    properties = ["application.runner.enabled=false"]
+    properties = ["application.runner.enabled=true"]
 )
 class AuthControllerTest(@Autowired val restTemplate: TestRestTemplate) {
     @Test
@@ -26,8 +28,26 @@ class AuthControllerTest(@Autowired val restTemplate: TestRestTemplate) {
     }
 
     @Test
-    fun testRegister() {
+    fun testShowRegisterPage() {
         val entity = restTemplate.getForEntity<String>("/register")
         assertThat(entity.statusCode).isEqualTo(HttpStatus.OK)
+    }
+
+    @Test
+    fun testLoginWithAdminUser() {
+        var form = LoginForm()
+        form.username = "admin"
+        form.password = "password"
+        val entity = restTemplate.postForEntity("/login", form, String::class.java)
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.FOUND)
+    }
+
+    @Test
+    fun testRegisterWithDuplicated() {
+        var form = RegisterForm()
+        form.username = "admin"
+        form.password = "admin"
+        val entity = restTemplate.postForEntity("/register", form, String::class.java)
+        assertThat(entity.statusCode).isEqualTo(HttpStatus.FOUND)
     }
 }
