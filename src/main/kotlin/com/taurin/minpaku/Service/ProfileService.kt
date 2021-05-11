f43207f042be4a0112e62d7986a216a537e04b75
Koichi Taura <taura.koichi@gmail.com>
2021-05-11 23:26:30 +0900

    add exception when fail to save

--- src/main/kotlin/com/taurin/minpaku/Service/ProfileService.kt
+++ src/main/kotlin/com/taurin/minpaku/Service/ProfileService.kt
@@ -1,6 +1,8 @@
 package com.taurin.minpaku.Service
 
+import com.taurin.minpaku.Exception.DBException
 import com.taurin.minpaku.Repository.ProfileRepository
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
@@ -9,13 +11,20 @@ class ProfileService {
     @Autowired
     private lateinit var profileRepository: ProfileRepository
 
+    private val logger = LoggerFactory.getLogger(AuthService::class.java)
+
     fun register(username: String, name: String, email: String, address: String, phone: String) {
-        profileRepository.saveWithUserName(
-            username,
-            name,
-            email,
-            address,
-            phone
-        )
+        try {
+            profileRepository.saveWithUserName(
+                username,
+                name,
+                email,
+                address,
+                phone
+            )
+        } catch (e: Exception) {
+            logger.warn("Profile Register fail with Unexpected Exception: ${e.message}")
+            throw DBException("登録出来ませんでした。")
+        }
     }
 }
