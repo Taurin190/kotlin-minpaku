81bf6ed6fcf95e0c81d965ade910b0701f9f9663
Koichi Taura <taura.koichi@gmail.com>
2021-05-26 07:56:06 +0900

    add response status on api

--- src/main/kotlin/com/taurin/minpaku/Presentation/Controller/Api/ReservationController.kt
+++ src/main/kotlin/com/taurin/minpaku/Presentation/Controller/Api/ReservationController.kt
@@ -26,6 +26,7 @@ class ReservationController {
     }
 
     @PostMapping("/add")
+    @ResponseStatus(HttpStatus.CREATED)
     fun register() : ApiResponse {
         return ApiResponse(
             ZonedDateTime.now(),
