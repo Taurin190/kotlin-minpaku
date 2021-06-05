61b1ee643c4385bba97c19cce7e5bfb95eb4d401
Koichi Taura <taura.koichi@gmail.com>
2021-06-06 07:50:48 +0900

    add test for get valid year function

--- src/test/kotlin/com/taurin/minpaku/Unit/DateUtilTest.kt
+++ src/test/kotlin/com/taurin/minpaku/Unit/DateUtilTest.kt
@@ -35,4 +35,46 @@ class DateUtilTest {
         val actual = DateUtil.getValidMonth("aaa")
         assertThat(actual).isEqualTo(currentMonth)
     }
+
+    @Test
+    fun testGetValidYear() {
+        val actual = DateUtil.getValidYear("2020")
+        assertThat(actual).isEqualTo(2020)
+    }
+
+    @Test
+    fun testGetValidYearLowerBoundaryWithValidValue() {
+        val actual = DateUtil.getValidYear("2000")
+        assertThat(actual).isEqualTo(2000)
+    }
+
+    @Test
+    fun testGetValidYearLowerBoundaryWithInValidValue() {
+        val cal = Calendar.getInstance()
+        val actual = DateUtil.getValidYear("1999")
+        val currentYear = cal.get(Calendar.YEAR)
+        assertThat(actual).isEqualTo(currentYear)
+    }
+
+    @Test
+    fun testGetValidYearHigherBoundaryWithValidValue() {
+        val actual = DateUtil.getValidYear("2040")
+        assertThat(actual).isEqualTo(2040)
+    }
+
+    @Test
+    fun testGetValidYearHigherBoundaryWithInValidValue() {
+        val cal = Calendar.getInstance()
+        val actual = DateUtil.getValidYear("2041")
+        val currentYear = cal.get(Calendar.YEAR)
+        assertThat(actual).isEqualTo(currentYear)
+    }
+
+    @Test
+    fun testGetValidYearWithInValidValue() {
+        val cal = Calendar.getInstance()
+        val actual = DateUtil.getValidYear("aaa")
+        val currentYear = cal.get(Calendar.YEAR)
+        assertThat(actual).isEqualTo(currentYear)
+    }
 }
