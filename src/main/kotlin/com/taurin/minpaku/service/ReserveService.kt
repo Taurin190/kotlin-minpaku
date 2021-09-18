e6fe5c058bf8083a366df79ec325f587fb771cb4
Koichi Taura <taura.koichi@gmail.com>
2021-09-18 09:59:39 +0900

    add service method to return domain collection model

--- src/main/kotlin/com/taurin/minpaku/service/ReserveService.kt
+++ src/main/kotlin/com/taurin/minpaku/service/ReserveService.kt
@@ -1,19 +1,25 @@
 package com.taurin.minpaku.service
 
+import com.taurin.minpaku.domain.reservation.Reservations
 import com.taurin.minpaku.infrastructure.Entity.Reservation
-import com.taurin.minpaku.infrastructure.exception.DBException
 import com.taurin.minpaku.infrastructure.Repository.ReserveRepository
+import com.taurin.minpaku.infrastructure.datasource.ReserveDataSource
+import com.taurin.minpaku.infrastructure.exception.DBException
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
 import java.sql.Date
+import java.time.LocalDate
 import java.util.*
 
 @Service
 class ReserveService {
     @Autowired
     private lateinit var reserveRepository: ReserveRepository
+
+    @Autowired
+    private lateinit var reserveDataSource: ReserveDataSource
 
     private val logger = LoggerFactory.getLogger(ReserveService::class.java)
 
@@ -29,6 +35,18 @@ class ReserveService {
                 Date.valueOf("$year-$padMonth-1"),
                 Date.valueOf("$year-$padMonth-$lastDayOfMonth")
             )
+    }
+
+    fun getReservationsInMonth(year: Int, month: Int): Reservations {
+        val cal = Calendar.getInstance()
+        cal.set(year, month + 1, 1)
+        cal.add(Calendar.DATE, -1)
+        val lastDayOfMonth = cal.get(Calendar.DATE)
+        val padMonth = "$month".padStart(2, '0')
+        return reserveDataSource.findAllByDuration(
+            LocalDate.parse("$year-$padMonth-01"),
+            LocalDate.parse("$year-$padMonth-$lastDayOfMonth")
+        )
     }
 
     @Transactional
--- src/test/kotlin/com/taurin/minpaku/unit/service/ReserveServiceTest.kt
+++ src/test/kotlin/com/taurin/minpaku/unit/service/ReserveServiceTest.kt
@@ -1,22 +1,32 @@
 package com.taurin.minpaku.unit.service
 
+import com.taurin.minpaku.domain.reservation.CheckInDateTime
+import com.taurin.minpaku.domain.reservation.CheckOutDateTime
+import com.taurin.minpaku.domain.reservation.Reservations
+import com.taurin.minpaku.domain.reservation.Title
+import com.taurin.minpaku.infrastructure.Entity.Book
+import com.taurin.minpaku.domain.reservation.Reservation as ReservationDomain
 import com.taurin.minpaku.infrastructure.Entity.Reservation
 import com.taurin.minpaku.infrastructure.exception.DBException
 import com.taurin.minpaku.infrastructure.Repository.ReserveRepository
+import com.taurin.minpaku.infrastructure.datasource.ReserveDataSource
 import com.taurin.minpaku.service.ReserveService
 import io.mockk.MockKAnnotations
 import io.mockk.every
 import io.mockk.impl.annotations.InjectMockKs
 import io.mockk.impl.annotations.MockK
 import io.mockk.verify
-import org.assertj.core.api.Assertions
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import java.time.LocalDateTime
 
 class ReserveServiceTest {
     @MockK
     private lateinit var reserveRepository: ReserveRepository
+
+    @MockK
+    private lateinit var reserveDataSource: ReserveDataSource
 
     @InjectMockKs
     private lateinit var reserveService: ReserveService
@@ -34,7 +44,30 @@ class ReserveServiceTest {
             )
         } returns listOf(reservation)
         val actual = reserveService.getReservationsByDuration(2021,  5)
-        Assertions.assertThat(actual.count()).isEqualTo(1)
+        assertThat(actual.count()).isEqualTo(1)
+    }
+
+    @Test
+    fun testGetReservationsInMonth() {
+        val reservations = Reservations()
+        val reservation = ReservationDomain(
+            Title("Test Taro"),
+            CheckInDateTime(
+                LocalDateTime.parse("2020-05-01T15:00:00"),
+            ),
+            CheckOutDateTime(
+                LocalDateTime.parse("2020-05-03T10:00:00"),
+            ),
+            null
+        )
+        reservations.append(reservation)
+
+        every {
+            reserveDataSource.findAllByDuration(any(), any())
+        } returns reservations
+
+        val actual = reserveService.getReservationsInMonth(2021, 5)
+        assertThat(actual.count()).isEqualTo(1)
     }
 
     @Test
