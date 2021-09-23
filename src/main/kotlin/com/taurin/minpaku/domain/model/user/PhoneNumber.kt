344729bfe8a14296a2be86b591ab76271b322c0e
Koichi Taura <taura.koichi@gmail.com>
2021-09-24 07:20:51 +0900

    add test and implement for phone number domain model

--- src/main/kotlin/com/taurin/minpaku/domain/model/user/PhoneNumber.kt
+++ src/main/kotlin/com/taurin/minpaku/domain/model/user/PhoneNumber.kt
@@ -3,4 +3,28 @@ package com.taurin.minpaku.domain.model.user
 class PhoneNumber(
     val value: String
 ) {
+    //TODO formと情報が重複しているためconfigにまとめる
+    private val regex = Regex(pattern = "^0[0-9/-]{11}")
+    init {
+        if (!validate()) {
+            throw IllegalStateException(
+                "電話番号は12文字以上、13文字以内の数字および-で、先頭は0を設定してください。"
+            )
+        }
+    }
+
+    override fun toString(): String {
+        return value
+    }
+
+    fun validate(): Boolean {
+        if (value.length !in 12..13) {
+            return false
+        }
+
+        if (!regex.containsMatchIn(value)) {
+            return false
+        }
+        return true
+    }
 }
---
+++ src/test/kotlin/com/taurin/minpaku/unit/domain/user/PhoneNumberTest.kt
@@ -0,0 +1,24 @@
+package com.taurin.minpaku.unit.domain.user
+
+import com.taurin.minpaku.domain.model.user.PhoneNumber
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.fail
+import org.junit.jupiter.api.Test
+
+class PhoneNumberTest {
+    @Test
+    fun testPhoneNumberWithValidNumber() {
+        val actual = PhoneNumber("080-1234-5678")
+        assertThat(actual.toString()).isEqualTo("080-1234-5678")
+    }
+
+    @Test
+    fun testPhoneNumberWithInvalidNumber() {
+        try {
+            PhoneNumber("08012345")
+            fail("不正な値に例外が発生しませんでした。")
+        } catch (e: IllegalStateException) {
+            assertThat(e.message).isEqualTo("電話番号は12文字以上、13文字以内の数字および-で、先頭は0を設定してください。")
+        }
+    }
+}
