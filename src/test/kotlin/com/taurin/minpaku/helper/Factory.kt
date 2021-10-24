f4147c272f0bebba7c65bb69ad739c46346cde32
Koichi Taura <taura.koichi@gmail.com>
2021-10-25 08:53:12 +0900

    make parameters nullable to skip

--- src/test/kotlin/com/taurin/minpaku/helper/Factory.kt
+++ src/test/kotlin/com/taurin/minpaku/helper/Factory.kt
@@ -4,13 +4,13 @@ import org.apache.poi.ss.formula.functions.T
 
 class Factory {
     companion object {
-        private val factoryMap = mutableMapOf<T, (Map<String, Any>) -> T>()
+        private val factoryMap = mutableMapOf<T, (Map<String, Any>?) -> T>()
 
-        fun define(staticClass: T, fn: (Map<String, Any>) -> T) {
+        fun define(staticClass: T, fn: (Map<String, Any>?) -> T) {
             factoryMap[staticClass] = fn
         }
 
-        fun make(staticClass: T, parameters: Map<String, Any>): T? {
+        fun make(staticClass: T, parameters: Map<String, Any>?): T? {
             return factoryMap[staticClass]?.invoke(parameters)
         }
     }
